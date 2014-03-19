7ff6944bc54996b2291e9fca1ba6befda65ceb12
sebastien-madru <sebastien@madru.fr>
2014-03-19 16:43:43 -0400

    Test

--- lib/rails-translate-routes.rb
+++ lib/rails-translate-routes.rb
@@ -415,7 +415,7 @@ ActionController::Base.class_eval do
   private
   # called by before_filter
   def set_locale_from_url
-    I18n.locale = params[RailsTranslateRoutes::LOCALE_PARAM_KEY]
+    #I18n.locale = params[RailsTranslateRoutes::LOCALE_PARAM_KEY]
     default_url_options = {RailsTranslateRoutes::LOCALE_PARAM_KEY => I18n.locale}
   end
 end
